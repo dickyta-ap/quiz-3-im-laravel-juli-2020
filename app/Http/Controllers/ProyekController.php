524028c6fe8dec27ade5c0a7e8a6a0d478b9bc5b
Dickyta Anggapraja Putra <dickyta-ap>
2020-08-10 21:49:58 +0700

    tugas laravel

---
+++ app/Http/Controllers/ProyekController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProyekController extends Controller
+{
+    public function index (){
+
+    }
+
+    public function create(){
+
+    }
+
+    public function store(){
+
+    }
+
+    
+}
